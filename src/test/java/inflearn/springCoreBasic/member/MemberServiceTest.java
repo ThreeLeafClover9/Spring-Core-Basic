d4ce119f878a519d0ccd84b317b9acf48704ad11
clover <threeleafclover9@gmail.com>
2023-05-17 13:35:02 +0900

    Test member domain

---
+++ src/test/java/inflearn/springCoreBasic/member/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package inflearn.springCoreBasic.member;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MemberServiceTest {
+    private final MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
