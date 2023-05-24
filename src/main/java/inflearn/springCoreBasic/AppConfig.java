c8fcaa3e8a530c71c2d0b957df1bf83e956a7a77
clover <threeleafclover9@gmail.com>
2023-05-24 17:08:41 +0900

    Refactor AppConfig

--- src/main/java/inflearn/springCoreBasic/AppConfig.java
+++ src/main/java/inflearn/springCoreBasic/AppConfig.java
@@ -1,6 +1,8 @@
 package inflearn.springCoreBasic;
 
+import inflearn.springCoreBasic.discount.DiscountPolicy;
 import inflearn.springCoreBasic.discount.FixDiscountPolicy;
+import inflearn.springCoreBasic.member.MemberRepository;
 import inflearn.springCoreBasic.member.MemberService;
 import inflearn.springCoreBasic.member.MemberServiceImpl;
 import inflearn.springCoreBasic.member.MemoryMemberRepository;
@@ -9,10 +11,18 @@ import inflearn.springCoreBasic.order.OrderServiceImpl;
 
 public class AppConfig {
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new FixDiscountPolicy();
     }
 }
