322169f4ffafac57c2bb1620579929efd93960ee
clover <threeleafclover9@gmail.com>
2023-05-27 22:07:23 +0900

    Test bean definition

---
+++ src/test/java/inflearn/springCoreBasic/beanDefinition/BeanDefinitionTest.java
@@ -0,0 +1,37 @@
+package inflearn.springCoreBasic.beanDefinition;
+
+import inflearn.springCoreBasic.AppConfig;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.config.BeanDefinition;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+class BeanDefinitionTest {
+
+    @Test
+    @DisplayName("class 파일로 빈 설정 메타정보 확인")
+    void findApplicationBeanByClass() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+
+    @Test
+    @DisplayName("xml 파일로 빈 설정 메타정보 확인")
+    void findApplicationBeanByXml() {
+        GenericXmlApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
+        String[] beanDefinitionNames = ac.getBeanDefinitionNames();
+        for (String beanDefinitionName : beanDefinitionNames) {
+            BeanDefinition beanDefinition = ac.getBeanDefinition(beanDefinitionName);
+            if (beanDefinition.getRole() == BeanDefinition.ROLE_APPLICATION) {
+                System.out.println("beanDefinitionName = " + beanDefinitionName + " beanDefinition = " + beanDefinition);
+            }
+        }
+    }
+}
