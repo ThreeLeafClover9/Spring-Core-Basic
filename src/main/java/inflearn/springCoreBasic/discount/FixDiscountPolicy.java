74f4a111d220cc0862acfbc89503540c1e091bf5
clover <threeleafclover9@gmail.com>
2023-06-04 17:07:13 +0900

    Feat custom @Qualifier

--- src/main/java/inflearn/springCoreBasic/discount/FixDiscountPolicy.java
+++ src/main/java/inflearn/springCoreBasic/discount/FixDiscountPolicy.java
@@ -1,10 +1,12 @@
 package inflearn.springCoreBasic.discount;
 
+import inflearn.springCoreBasic.annotation.SubDiscountPolicy;
 import inflearn.springCoreBasic.member.Grade;
 import inflearn.springCoreBasic.member.Member;
 import org.springframework.stereotype.Component;
 
 @Component
+@SubDiscountPolicy
 public class FixDiscountPolicy implements DiscountPolicy {
     private final int discountFixAmount = 1000;
 
