c6d66ba6180df62bdb766cb0f35e37f44254247e
clover <threeleafclover9@gmail.com>
2023-06-06 17:49:25 +0900

    Test dependency lookup

--- src/test/java/inflearn/springCoreBasic/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/inflearn/springCoreBasic/scope/SingletonWithPrototypeTest1.java
@@ -32,19 +32,20 @@ class SingletonWithPrototypeTest1 {
         assertThat(count1).isEqualTo(1);
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+        private final ApplicationContext ac;
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
+        public ClientBean(ApplicationContext ac) {
+            this.ac = ac;
         }
 
         public int logic() {
+            PrototypeBean prototypeBean = ac.getBean(PrototypeBean.class);
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
